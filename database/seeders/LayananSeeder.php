340473edb8e0443adcae0fbcc285655a1e55be3b
Oktavianipratiwi <okta05532@gmail.com>
2024-08-07 23:31:01 +0700

    update seeder

--- database/seeders/LayananSeeder.php
+++ database/seeders/LayananSeeder.php
@@ -17,13 +17,13 @@ class LayananSeeder extends Seeder
     {
         // Contoh data layanan
         $layanan = [
-            ['jenis_layanan' => 'Cuci 2 Hari', 'harga' => '4000', 'jenis_satuan' => 'kiloan'],
-            ['jenis_layanan' => 'Cuci Setrika 2 Hari', 'harga' => '6000', 'jenis_satuan' => 'kiloan'],
-            ['jenis_layanan' => 'Setrika 2 Hari', 'harga' => '4000', 'jenis_satuan' => 'kiloan'],
-            ['jenis_layanan' => 'Cuci Ekspress 12 Jam', 'harga' => '8000', 'jenis_satuan' => 'kiloan'],
-            ['jenis_layanan' => 'Selimut 2 Hari', 'harga' => '8000', 'jenis_satuan' => 'satuan'],
-            ['jenis_layanan' => 'Sprei 2 Hari', 'harga' => '10000', 'jenis_satuan' => 'satuan'],
-            ['jenis_layanan' => 'Handuk 2 Hari', 'harga' => '5000', 'jenis_satuan' => 'satuan'],
+            ['jenis_layanan' => 'Cuci 2 Hari', 'harga' => '4000', 'jenis_satuan' => 'kiloan','durasi_layanan' => '2 hari'],
+            ['jenis_layanan' => 'Cuci Setrika 2 Hari', 'harga' => '6000', 'jenis_satuan' => 'kiloan','durasi_layanan' => '2 hari'],
+            ['jenis_layanan' => 'Setrika 2 Hari', 'harga' => '4000', 'jenis_satuan' => 'kiloan','durasi_layanan' => '2 hari'],
+            ['jenis_layanan' => 'Cuci Ekspress 12 Jam', 'harga' => '8000', 'jenis_satuan' => 'kiloan','durasi_layanan' => '12 jam'],
+            ['jenis_layanan' => 'Selimut 2 Hari', 'harga' => '8000', 'jenis_satuan' => 'satuan','durasi_layanan' => '2 hari'],
+            ['jenis_layanan' => 'Sprei 2 Hari', 'harga' => '10000', 'jenis_satuan' => 'satuan','durasi_layanan' => '2 hari'],
+            ['jenis_layanan' => 'Handuk 2 Hari', 'harga' => '5000', 'jenis_satuan' => 'satuan','durasi_layanan' => '2 hari'],
         ];
         // Masukkan data layanan ke dalam tabel
         foreach ($layanan as $p) {
